6cb3b58aa12824c1f674a8dd611433e2962d4017
gpires97 <162461724+gpires97@users.noreply.github.com>
2026-09-17 08:42:13 -0300

    Add files via upload

---
+++ adicionando_notas.js
@@ -0,0 +1,9 @@
+//Manipulação com os arrays,
+//inserir novos dados
+const notas = [10, 6, 8];
+notas = [];
+notas.push(7);
+//método que coloca mais
+//um elemento no array
+const media = (notas[0]+notas[1]+notas[2]/
+    notas.length)
